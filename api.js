069086bf089e54977dd660e2ffd0ee3e3fee91f5
florian hostachy <2032399@cegeprdl.ca>
2023-04-21 09:33:10 -0400

    initial commit

---
+++ api.js
@@ -0,0 +1,66 @@
+const express = require('express')
+const mysql = require('mysql')
+const cors = require('cors')
+const app = express()
+const port = process.env.PORT || 8080
+
+app.listen(port, () => console.log('Listen on port ' + port))
+//Mysql
+
+app.use(function (req, res, next) {
+  res.locals.connection = mysql.createConnection({
+    connectionLimit: 10,
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'CO2',
+  })
+  res.locals.connection.connect()
+  next()
+})
+const corsOptions = {
+  origin: '*',
+  method: 'GET,PUT,POST,DELETE,OPTIONS',
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions))
+
+// JSON body parser, there is no `extended` option here
+app.use(express.json())
+
+// URL-encoded body parser
+app.use(express.urlencoded({ limit: '20mb', extended: true }))
+
+
+/////////////////////////Connexion////////////////////////
+
+
+//API CONNEXION
+
+app.post('/login', function (req, res) {
+  const { username, password } = req.body
+  console.log (username,password)
+  // SQL query to check if user exists and password is correct
+  const query = `SELECT * FROM utilisateur WHERE login = '${username}' AND password = '${password}'`
+
+  // Execute the SQL query
+  res.locals.connection.query(query, function (err, rows) {
+    if (err) {
+      console.log(err)
+      res.status(500).send('Internal server error')
+    } else if (rows.length == 0) {
+      res.status(401).send('Invalid username or password')
+    } else {
+      const utilisateur = rows[0]
+      // Successful login, send back the user data
+      res.send({
+        id: utilisateur.id,
+        login: utilisateur.login,
+        nom: utilisateur.nom,
+        prenom: utilisateur.prenom,
+        idRole: utilisateur.idRole,
+      })
+    }
+  })
+})
